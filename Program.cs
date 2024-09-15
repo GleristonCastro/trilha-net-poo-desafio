1c1950ef3183914eddcc3401776fe77f268b6d7a
Gleriston <xpanium@gmail.com>
2024-09-15 00:58:12 -0300

    In response to the third POO challenge

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone Nokia = new Nokia(numero: "123456789", modelo: "Modelo 1", imei: "123456789", memoria: 128);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iPhone = new Iphone(numero: "987654321", modelo: "Modelo 2", imei: "987654321", memoria: 128);
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("Telegram");
